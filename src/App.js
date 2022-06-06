774dfcdcd323dc6563ac6334edd1a2a1f9313621
Rohit Singh <95858652+rohitsinghrajpoot@users.noreply.github.com>
2022-06-06 16:48:43 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+// import "Login" from "./Pages/Login"
+// import "Home" from "./Pages/Home"
+function App() {
+  return <div className="App">{/* <Login />
+  <Home />  */}</div>;
+}
+export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  return <div>{
+    <h3> Login<h3/>
+  }</div>;
+};
+
+export default Home;
